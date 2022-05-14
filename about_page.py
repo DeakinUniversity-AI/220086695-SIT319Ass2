c312a8faaeb32a259a6b6f4d95c4264d513ab0c4
PeibingGu <87626621+PeibingGu@users.noreply.github.com>
2022-05-14 21:17:14 +1000

    Update about_page.py

--- about_page.py
+++ about_page.py
@@ -7,13 +7,13 @@ def show_about_page():
         show_predict_page()
 
     st.title('A Deep Learning solution to reduce recycling contamination')
-    st.write('*1. The image dataset comes the web resources below:*')
+    st.write('\n\n**1. The image dataset comes the web resources below:**')
     st.write('https://www.kaggle.com/datasets/hseyinsaidkoca/recyclable-solid-waste-dataset-on-5-background-co')
     st.write('https://www.kaggle.com/code/tszheilau/0-9102-efficientnet-b0-svm-transfer-learning/data')
     st.write('https://www.kaggle.com/datasets/ashwinshrivastav/most-common-recyclable-and-nonrecyclable-objects')
     st.write('https://www.bing.com/images/trending')
 
-    st.write('There are 1125 images in total collected across 10 different classes:')
+    st.write('\nThere are 1125 images in total collected across 10 different classes:')
     st.write('''
     0 : Recyclable Paper\n
     1 : Recyclable Cardboard\n
@@ -27,14 +27,17 @@ def show_about_page():
     9 : Unrecyclable Polystyrene\n
     ''')
 
-    st.write('*2. Convolutional Neural Network*')
+    st.write('\n\n**2. Convolutional Neural Network**')
     st.write('An input pipeline applied to the CNN model, the images were resized and transformed into certain size.')
     st.write('Then the sample images was split into training and test based on a ratio of 80:20, fitting and evaluating')
     st.write('the model.')
 
-    st.write('*3. Performance of the model*')
+    st.write('\n\n**3. Performance of the model**')
     st.write('After improvements on the model, the validation accuray was as much as 82.47%.')
 
     st.write('\n\n')
-    st.write('Code can be found at:')
-    st.write('')
+    st.write('The code can be found at: ')
+    st.write('GitHub:  https://github.com/DeakinUniversity-AI/220086695-SIT319Ass2.git')
+    st.write('\n\n**IMPORTANT**')
+    st.write('\n\nFor GitHub limits the single file size up to 100MB, but the pickle file generate here is quite large.')
+    st.write('So please read the information in README.md in repository to download `saved_model.pkl` manually, and put it into the project folder')
